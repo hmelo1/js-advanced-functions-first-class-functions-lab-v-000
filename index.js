e11becbb79941489314df586707284817afd3885
Eddie Melo <heriberto@melo.nyc>
2018-05-04 14:32:45 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -9,6 +9,6 @@ const returnLastTwoDrivers = function(drivers){
 };
 
 const selectingDrivers = [
-  returnFirstTwoDrivers(),
-  returnLastTwoDrivers()
+  returnFirstTwoDrivers,
+  returnLastTwoDrivers
 ];
